44fcff7c1272ef714b7e4afd5f536cc68d35b6d1
ed-fish <edward.fish@surrey.ac.uk>
2021-10-02 16:23:51 +0100

    env testing on production

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 var express = require('express');
 
 var app = express();
-app.get("public/assets/js/tsne.js", (req, res) => {
-     fs.readFile("public/assets/js/tsne.js", (err, file) => {
-        res.send(file.replace("API_KEY", process.env.API_KEY));
-    });
-  });
+// app.get("public/assets/js/tsne.js", (req, res) => {
+//      fs.readFile("public/assets/js/tsne.js", (err, file) => {
+//         res.send(file.replace("API_KEY", process.env.API_KEY));
+//     });
+//   });
 
 app.use(express.static('public'));
 
